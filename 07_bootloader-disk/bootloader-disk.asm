f4a94d28502724ae845dbe05e6ed42e37530207b
Preston From <pfrom@spotx.tv>
2018-10-11 14:39:08 -0600

    Finishing chapter 7 and adding lots of comments

--- 07_bootloader-disk/bootloader-disk.asm
+++ 07_bootloader-disk/bootloader-disk.asm
@@ -1,25 +1,36 @@
-mov bp, 0x8000  ; Set stack base pointer far away
-mov sp, bp      ; Set stack pointer to base pointer because stack is empty
+mov bp, 0x8000                  ; Set stack base pointer far away
+mov sp, bp                      ; Set stack pointer to base pointer because stack is empty
 
-mov bx, 0x9000  ;
-mov dh, 2       ; Read 2 sectors
+mov bx, 0x9000                  ; This tells the bios where to read the sector data to in memory.
+                                ; It indirectly sets ES (extra segment register) to 0x9000.
+                                ; You can change this and also change the 0x9000 below.
+
+mov dh, 3                       ; Read 3 sectors
 call disk_load
 
-mov dx, [0x9000]
+mov dx, [0x9000]                ; Print the first two bytes from sector 2
 call prehex
 
-mov dx, [0x9000 + 512]
+mov dx, [0x9000 + 512]          ; Print the first two bytes from sector 3
 call prehex
 
-jmp $
+mov dx, [0x9000 + 512 + 512]    ; Print the first two bytes from sector 4
+call prehex
 
+jmp $ ; Loop forever
+
+; Include all our print functions
 %include "../05_bootloader-functions-print-hex/print_hex.asm"
 %include "../05_bootloader-functions-print-hex/boot_include_file.asm"
+
+; Also include the functions to read from sectors
 %include "bootloader-sect-disk.asm"
 
+; Make sure our bootsector looks right!
 times 510 - ($ - $$) db 0
 dw 0xaa55
 
-times 256 dw 0xdada
-times 256 dw 0xface
+times 256 dw 0xdada             ; Fill sector 2 with 0xdada
+times 256 dw 0xface             ; Fill sector 3 with 0xface
+times 256 dw 0x1010             ; Fill sector 3 with 0x1010
 
--- 07_bootloader-disk/bootloader-sect-disk.asm
+++ 07_bootloader-disk/bootloader-sect-disk.asm
@@ -1,37 +1,40 @@
 disk_load:
     pusha                   ; Save all general purpose registers
-    push dx                 ; We'll 
+    push dx                 ; Save dx since it contains our input value
 
-    mov ah, 0x02
-    mov al, dh
-    mov cl, 0x02
+    mov ah, 0x02            ; 0x02 is needed to call the BIOS read sector function
+    mov al, dh              ; This tells the BIOS how many sectors to read
+    mov cl, 0x02            ; cl tells the BIOS which sector to start the read at. 
+                            ; (1 is the boot sector so we start at the second sector.)
+    mov ch, 0x00            ; ch contains the cylinder to read
+    mov dh, 0x00            ; dh contains the head number
 
-    mov ch, 0x00
-    mov dh, 0x00
+    int 0x13                ; BIOS interrupt
+    jc disk_error           ; Jump to disk_error if there is an error
+                            ; (An error is indicated by setting the cary flag)
 
-    int 0x13
-    jc disk_error
+    pop dx                  ; Restore dx
+    cmp al, dh              ; Check how many sectors were actually read (al) to the
+                            ; the expected number to read (dh)
+    jne sectors_error       ; If they're not the same, print an error
 
-    pop dx
-    cmp al, dh
-    jne sectors_error
-
-    popa
+    popa                    ; Otherwise, restore regisers and return
     ret
 
 disk_error:
-    mov bx, DISK_ERROR
-    call preprint
-    mov dh, ah
-    call prehex
+    mov bx, DISK_ERROR      ; Set bx to point at the DISK_ERROR string
+    call preprint           ; And print it.
+    mov dh, ah              ; ah contains the error code and dl contains the disk drive
+                            ; that had the error, so dx = dh (error) and dl (which drive)
+    call prehex             ; Print as hex
     jmp disk_loop
 
 sectors_error:
-    mov bx, SECTORS_ERROR
-    call print
+    mov bx, SECTORS_ERROR   ; Just print it!
+    call preprint
 
 disk_loop:
-    jmp $
+    jmp $                   ; Nothing else we can do
 
 DISK_ERROR: db "Disk read error", 0
 SECTORS_ERROR: db "Incorrect number of sectors read", 0
